5d9a1f6018cffcb689267545417ee385602d6627
Julia Beltrao <julia.ramos.beltrao@gmail.com>
2016-06-22 18:52:55 +0300

    Adding script to generate plots and graph images

---
+++ run_plot_graphs.py
@@ -0,0 +1,13 @@
+import pickle
+from generate_nodePairs import plot_graph
+from draw_graph import draw_network
+
+toy_networks = ["toy_lattice.pickle"]
+cities_networks = ["des_moines_100.pickle"]
+
+for network in toy_networks:
+
+	f = "toy_networks/" + network
+	network = pickle.load(open(f))
+	plot_graph(network)
+	#draw_network(network)
