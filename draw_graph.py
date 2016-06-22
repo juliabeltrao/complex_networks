76264588755e9509c19970d9bb73ba121320e15b
Julia Beltrao <julia.ramos.beltrao@gmail.com>
2016-06-22 18:53:46 +0300

    Adding module to generate images of the networks

---
+++ draw_graph.py
@@ -0,0 +1,13 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from greedy_navigator import map_projection
+
+def draw_network(network):
+
+	pos={}
+	for n in network.nodes():
+		pos[n] = np.array(map_projection(network.node[n]['lat'], network.node[n]['lon']))
+
+	nx.draw(network, pos)
+	plt.show()
